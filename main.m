8a89ea3ea12342fc9eba308c3a3d98084c5ca892
Jeeva Keshav <keshav.jeeva88@gmail.com>
2019-11-19 11:24:54 +0530

    CP addition

--- main.m
+++ main.m
@@ -5,19 +5,30 @@ subcarriers     = 4096;                 %Total no. of sub-carriers
 u_subcarriers   = 3072;                 %No. of useful sub-carriers
 symbols         = 14;
 n=u_subcarriers * symbols;              %No. of bits in PRB
-type=0;                                 %type 0/1/2/3/4 -> BPSK/QPSK/16QAM/64QAM
+type=2;                                 %type 0/1/2/3/4 -> BPSK/QPSK/16QAM/64QAM
 bits = gen_bits(n,type);                %generate bits
 tx_map=map_mod(bits,n,type);               %Modulation
 val = reshape(tx_map,[u_subcarriers,symbols]);
 tx_grid = ifft(val, subcarriers);
-tx_val = reshape(tx_grid,[1,subcarriers*symbols]);
+%tx_ch = reshape(tx_grid,[1,numel(tx_grid)]);
+
+%CP_addition
+tx_val = [tx_grid((4096-351):4096,1).' tx_grid(:,1).'];
+for k = 2:14
+    tx_val = [tx_val tx_grid((4096-287):4096,k).' tx_grid(:,k).'];
+end
 
 
-
-%Rx chain
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Receiver Chain%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 rx_val      = tx_val;
-rx_grid     = reshape(rx_val,[subcarriers,symbols]);
+%CP_strip
+rx_cp = rx_val((1:4096)+352);
+for k= 2:14
+    rx_cp = [rx_cp rx_val((352+4384*(k-1))+(1:4096))];
+end
+
+rx_grid     = reshape(rx_cp,[subcarriers,symbols]);
 rx_fft      = fft(rx_grid,subcarriers);
 rx_u_val    = rx_fft(1:u_subcarriers,:);
 
