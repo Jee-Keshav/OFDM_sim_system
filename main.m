7aefabce66f4cfa961c5ad3a6e05b283e97cfc15
Jeeva Keshav <keshav.jeeva88@gmail.com>
2019-12-01 11:03:24 +0530

    Made subtle changes for estimation

--- main.m
+++ main.m
@@ -66,8 +66,9 @@ for sym=1:dmrs_symbol1
     H(1:u_subcarriers,sym) = H_symbol;
 end
 
-H_symbol(1:2:u_subcarriers)
-for k=2:u_subcarriers-1
+H_symbol(1:2:u_subcarriers-1,1) = rx_u_val(1:2:u_subcarriers-1,dmrs_symbol1+10)./ pilot1(1:2:u_subcarriers-1,1);
+
+for k=2:2:u_subcarriers-1
     H_symbol(k-1,1)     = rx_u_val(k-1,dmrs_symbol1+1);
     H_symbol(k,1)       = (rx_u_val(k-1,dmrs_symbol1+1)+rx_u_val(k+1,dmrs_symbol1+1))/2;
 end
@@ -77,7 +78,7 @@ for sym = dmrs_symbol1+1 : dmrs_symbol2
     H(1:u_subcarriers,sym) = H_symbol;
 end
 
-for k=2:u_subcarriers-1
+for k=2:2:u_subcarriers-1
     H_symbol(k-1,1)     = rx_u_val(k-1,dmrs_symbol2+1);
     H_symbol(k,1) = (rx_u_val(k-1,dmrs_symbol2+1)+rx_u_val(k+1,dmrs_symbol2+1))/2;
 end
